caa776642782b4df34494102d0b63c42ecb59eb2
natansevero <natanseverolp@gmail.com>
2016-10-19 12:55:50 -0200

    Adicionando métodos de criae table e inserir na tabela

--- src/com/ifpb/jdbc/controller/Dao.java
+++ src/com/ifpb/jdbc/controller/Dao.java
@@ -23,14 +23,33 @@ public class Dao {
         conn = ConFactory.getConnection(url, user, password);
     }
     
+    /*
+        Conograma de métodos:
+            -Um com Statement, criando tabela
+            -Um com Statemnt, adionando dados
+            -Um com Stament, fazendo consulta, podendo manipular um ResultSet
+            -Um com PreparedStatement, fazendo uma consulta pré-compilada
+            -Um com CallableStatement, com procedimento armazenado
+            -Um com ResultSetMetaData, de um consulta
+            -Um com DataBaseMetaData, de uma conexão
+            -JdbcRowSet
+            -CachedRowSet
+            -JoinRowSet
+    
+    */
+    
     public boolean createTablePessoa() throws SQLException{
         Statement stmt = conn.createStatement();
         String sql = "create table pessoa(cpf varchar(14), nome varchar(50) not null, idade integer not null, primary key(cpf))";
         
-        if(stmt.executeQuery(sql) == null) return false;
-        
-        return true;
+        return stmt.executeUpdate(sql) > 0;
     }
     
+    public boolean insertTablePessoa(String cpf, String nome, String idade) throws SQLException {
+        Statement stmt = conn.createStatement();
+        String sql = "insert into pessoa values ('"+ cpf +"','"+ nome +"',"+idade+") ";
+        
+        return stmt.executeUpdate(sql) > 0;
+    }
     
 }
